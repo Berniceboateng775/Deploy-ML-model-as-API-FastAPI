13408aa98600202767faa684bb7f8a5913f7331e
Bernice Boateng <berniceboateng775@gmail.com>
2026-03-19 11:27:43 +0000

    inputs defined for model to predict

--- model/ml_api.py
+++ model/ml_api.py
@@ -17,3 +17,24 @@ class model_input(BaseModel):
 
 # Load the model
 diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
+
+@app.post('/diabetes_prediction')
+def diabetes_pred(input_parameters : model_input):
+    input_data = input_parameters.json()
+    input_dictionary = json.loads(input_data)
+    
+    preg = input_dictionary['Pregnancies']
+    glu = input_dictionary['Glucose']
+    bp = input_dictionary['BloodPressure']
+    skin = input_dictionary['SkinThickness']
+    insulin = input_dictionary['Insulin']
+    bmi = input_dictionary['BMI']
+    dpf = input_dictionary['DiabetesPedigreeFunction']
+    age = input_dictionary['Age']
+
+    input_list = [preg, glu, bp, skin, insulin, bmi, dpf, age]
+    prediction = diabetes_model.predict([input_list])
+    if prediction[0] == 0:
+        return 'The person is not diabetic'
+    else:
+        return 'The person is diabetic'
